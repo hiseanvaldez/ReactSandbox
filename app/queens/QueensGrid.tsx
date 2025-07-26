d41b828f14673489f3e9b1bfcd3d591ee91e7f73
Shan Valdez <hisean.valdez.dev@gmail.com>
2025-07-27 02:28:27 +0800

    Show queen on grid.

--- app/queens/QueensGrid.tsx
+++ app/queens/QueensGrid.tsx
@@ -1,3 +1,4 @@
+import { FaRegChessQueen } from "react-icons/fa6";
 import { colors } from "../constants/colors";
 import { Puzzle } from "../models/puzzle";
 
@@ -13,25 +14,32 @@ const QueensGrid = ({ puzzle }: { puzzle: Puzzle }) => {
       }}
     >
       {Array.from({ length: size * size }, (_, i) => {
+        let isQueen = false;
         const x = i % size;
         const y = Math.floor(i / size);
 
-        const hive = puzzle.hives.find((h) =>
-          h.blocks.some(([bx, by]) => bx === x && by === y)
-        );
+        const hive = puzzle.hives.find((h) => {
+          if (h.queen[0] === x && h.queen[1] === y) {
+            isQueen = true;
+          }
+
+          return h.blocks.some(([bx, by]) => bx === x && by === y);
+        });
 
         const bgColor = hive?.color ? colors[hive.color] : "white";
 
         return (
           <div
             key={`${x}-${y}`}
-            className="box-border border border-gray-900 "
+            className="box-border flex items-center justify-center border border-gray-900"
             style={{
               width: `${cellSize}px`,
               height: `${cellSize}px`,
               backgroundColor: bgColor,
             }}
-          />
+          >
+            {isQueen ? <FaRegChessQueen size={32} color="#333" /> : null}
+          </div>
         );
       })}
     </div>
